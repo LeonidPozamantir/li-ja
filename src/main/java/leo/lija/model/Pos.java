271de984ba76d49756aa1cb4b8f575b955bed59e
LeonidPozamantir <leonid.pozamantir@gmail.com>
2025-03-10 00:35:53 +0200

    Pos partial improve

--- src/main/java/leo/lija/model/Pos.java
+++ src/main/java/leo/lija/model/Pos.java
@@ -1,8 +1,8 @@
 package leo.lija.model;
 
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -12,40 +12,103 @@ import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+@AllArgsConstructor
 @EqualsAndHashCode
 public class Pos {
 
     private final int x;
     private final int y;
-    private static final Set<Integer> values = IntStream.rangeClosed(1, 8)
+
+    public Optional<Pos> shiftUp(int n) {
+        return bounds.contains(y + n) ? Optional.of(new Pos(x, y + n)) : Optional.empty();
+    }
+
+    public Optional<Pos> shiftDown(int n) {
+        return bounds.contains(y - n) ? Optional.of(new Pos(x, y - n)) : Optional.empty();
+    }
+
+    public Optional<Pos> shiftLeft(int n) {
+        return bounds.contains(x - n) ? Optional.of(new Pos(x - n, y)) : Optional.empty();
+    }
+
+    public Optional<Pos> shiftRight(int n) {
+        return bounds.contains(x + n) ? Optional.of(new Pos(x + n, y)) : Optional.empty();
+    }
+
+    public boolean isHoriz(Pos other) {
+        return y == other.y;
+    }
+
+    public boolean isVert(Pos other) {
+        return x == other.x;
+    }
+
+    public boolean isDiag(Pos other) {
+        return Math.abs(x - other.x) == Math.abs(y - other.y);
+    }
+
+    public boolean isLinear(Pos other) {
+        return isHoriz(other) || isVert(other) || isDiag(other);
+    }
+
+    public List<Pos> multShiftUp(int n) {
+        return multShift(n, Pos::shiftUp);
+    }
+
+    public List<Pos> multShiftDown(int n) {
+        return multShift(n, Pos::shiftDown);
+    }
+
+    public List<Pos> multShiftLeft(int n) {
+        return multShift(n, Pos::shiftLeft);
+    }
+
+    public List<Pos> multShiftRight(int n) {
+        return multShift(n, Pos::shiftRight);
+    }
+
+    public List<Pos> multShift(int n, UnaryOperator<Optional<Pos>> dir) {
+        return expandN(n, Optional.of(this), dir)
+                .stream()
+                .map(Optional::get)
+                .toList();
+    }
+
+    public String xToString() {
+        return String.valueOf((char) x);
+    }
+
+    public String yToString() {
+        return String.valueOf(y);
+    }
+
+    @Override
+    public String toString() {
+        return xToString() + yToString();
+    }
+
+    private List<Optional<Pos>> expandN(int n, Optional<Pos> op, UnaryOperator<Optional<Pos>> dir) {
+        List<Optional<Pos>> result = List.of();
+        Optional<Pos> cur = op;
+        while (n >= 0 && cur.isPresent()) {
+            result.add(cur);
+            cur = dir.apply(cur);
+            n--;
+        }
+        return result;
+    }
+
+
+    private static final Set<Integer> bounds = IntStream.rangeClosed(1, 8)
             .boxed()
             .collect(Collectors.toSet());
 
-    private static final Map<String, Pos> allKeys;
-    static {
-        allKeys = new HashMap<>();
-        for (int i: values) {
-            for (int j: values) {
-                allKeys.put(i + "" + j, new Pos(i, j));
-            }
-        }
+    public static Optional<Pos> at(int x, int y) {
+        if (bounds.contains(x) && bounds.contains(y)) return Optional.of(new Pos(x, y)); else return Optional.empty();
     }
 
-
-    public Pos(int x, int y) {
-        if (!values.contains(x) || !values.contains(y)) {
-            throw new IllegalArgumentException("Invalid position");
-        }
-        this.x = x;
-        this.y = y;
-    }
-    public static Pos at(String s) {
-        char c1 = s.charAt(0);
-        char c2 = s.charAt(1);
-        if (!Character.isDigit(c2) || !Character.isLetter(c1)) {
-            throw new IllegalArgumentException("Invalid position");
-        }
-        return allKeys.get(c1 - 'a' + 1 + "" + c2);
+    public static Pos atUnsafe(int x, int y) {
+        return new Pos(x, y);
     }
 
     public static Optional<Pos> shiftUp(Optional<Pos> op) {
@@ -94,83 +157,80 @@ public class Pos {
         return result;
     }
 
-    public Optional<Pos> shiftUp(int n) {
-        return values.contains(y + n) ? Optional.of(new Pos(x, y + n)) : Optional.empty();
-    }
+    public static final Pos A1 = new Pos(1, 1);
+    public static final Pos A2 = new Pos(1, 2);
+    public static final Pos A3 = new Pos(1, 3);
+    public static final Pos A4 = new Pos(1, 4);
+    public static final Pos A5 = new Pos(1, 5);
+    public static final Pos A6 = new Pos(1, 6);
+    public static final Pos A7 = new Pos(1, 7);
+    public static final Pos A8 = new Pos(1, 8);
+    public static final Pos B1 = new Pos(2, 1);
+    public static final Pos B2 = new Pos(2, 2);
+    public static final Pos B3 = new Pos(2, 3);
+    public static final Pos B4 = new Pos(2, 4);
+    public static final Pos B5 = new Pos(2, 5);
+    public static final Pos B6 = new Pos(2, 6);
+    public static final Pos B7 = new Pos(2, 7);
+    public static final Pos B8 = new Pos(2, 8);
+    public static final Pos C1 = new Pos(3, 1);
+    public static final Pos C2 = new Pos(3, 2);
+    public static final Pos C3 = new Pos(3, 3);
+    public static final Pos C4 = new Pos(3, 4);
+    public static final Pos C5 = new Pos(3, 5);
+    public static final Pos C6 = new Pos(3, 6);
+    public static final Pos C7 = new Pos(3, 7);
+    public static final Pos C8 = new Pos(3, 8);
+    public static final Pos D1 = new Pos(4, 1);
+    public static final Pos D2 = new Pos(4, 2);
+    public static final Pos D3 = new Pos(4, 3);
+    public static final Pos D4 = new Pos(4, 4);
+    public static final Pos D5 = new Pos(4, 5);
+    public static final Pos D6 = new Pos(4, 6);
+    public static final Pos D7 = new Pos(4, 7);
+    public static final Pos D8 = new Pos(4, 8);
+    public static final Pos E1 = new Pos(5, 1);
+    public static final Pos E2 = new Pos(5, 2);
+    public static final Pos E3 = new Pos(5, 3);
+    public static final Pos E4 = new Pos(5, 4);
+    public static final Pos E5 = new Pos(5, 5);
+    public static final Pos E6 = new Pos(5, 6);
+    public static final Pos E7 = new Pos(5, 7);
+    public static final Pos E8 = new Pos(5, 8);
+    public static final Pos F1 = new Pos(6, 1);
+    public static final Pos F2 = new Pos(6, 2);
+    public static final Pos F3 = new Pos(6, 3);
+    public static final Pos F4 = new Pos(6, 4);
+    public static final Pos F5 = new Pos(6, 5);
+    public static final Pos F6 = new Pos(6, 6);
+    public static final Pos F7 = new Pos(6, 7);
+    public static final Pos F8 = new Pos(6, 8);
+    public static final Pos G1 = new Pos(7, 1);
+    public static final Pos G2 = new Pos(7, 2);
+    public static final Pos G3 = new Pos(7, 3);
+    public static final Pos G4 = new Pos(7, 4);
+    public static final Pos G5 = new Pos(7, 5);
+    public static final Pos G6 = new Pos(7, 6);
+    public static final Pos G7 = new Pos(7, 7);
+    public static final Pos G8 = new Pos(7, 8);
+    public static final Pos H1 = new Pos(8, 1);
+    public static final Pos H2 = new Pos(8, 2);
+    public static final Pos H3 = new Pos(8, 3);
+    public static final Pos H4 = new Pos(8, 4);
+    public static final Pos H5 = new Pos(8, 5);
+    public static final Pos H6 = new Pos(8, 6);
+    public static final Pos H7 = new Pos(8, 7);
+    public static final Pos H8 = new Pos(8, 8);
 
-    public Optional<Pos> shiftDown(int n) {
-        return values.contains(y - n) ? Optional.of(new Pos(x, y - n)) : Optional.empty();
-    }
-
-    public Optional<Pos> shiftLeft(int n) {
-        return values.contains(x - n) ? Optional.of(new Pos(x - n, y)) : Optional.empty();
-    }
-
-    public Optional<Pos> shiftRight(int n) {
-        return values.contains(x + n) ? Optional.of(new Pos(x + n, y)) : Optional.empty();
-    }
-
-    public boolean isHoriz(Pos other) {
-        return y == other.y;
-    }
-
-    public boolean isVert(Pos other) {
-        return x == other.x;
-    }
-
-    public boolean isDiag(Pos other) {
-        return Math.abs(x - other.x) == Math.abs(y - other.y);
-    }
-
-    public boolean isLinear(Pos other) {
-        return isHoriz(other) || isVert(other) || isDiag(other);
-    }
-
-    public List<Pos> multShiftUp(int n) {
-        return multShift(n, Pos::shiftUp);
-    }
-
-    public List<Pos> multShiftDown(int n) {
-        return multShift(n, Pos::shiftDown);
-    }
-
-    public List<Pos> multShiftLeft(int n) {
-        return multShift(n, Pos::shiftLeft);
-    }
-
-    public List<Pos> multShiftRight(int n) {
-        return multShift(n, Pos::shiftRight);
-    }
-
-    public List<Pos> multShift(int n, UnaryOperator<Optional<Pos>> dir) {
-        return expandN(n, Optional.of(this), dir)
-                .stream()
-                .map(Optional::get)
-                .toList();
-    }
-
-    public String xToString() {
-        return String.valueOf(x);
-    }
-
-    public String yToString() {
-        return String.valueOf(y);
-    }
-
-    @Override
-    public String toString() {
-        return xToString() + yToString();
-    }
-
-    private List<Optional<Pos>> expandN(int n, Optional<Pos> op, UnaryOperator<Optional<Pos>> dir) {
-        List<Optional<Pos>> result = List.of();
-        Optional<Pos> cur = op;
-        while (n >= 0 && cur.isPresent()) {
-            result.add(cur);
-            cur = dir.apply(cur);
-            n--;
-        }
-        return result;
-    }
+    private static final Map<String, Pos> allKeys = Map.ofEntries(
+            Map.entry("a1", A1), Map.entry("a2", A2), Map.entry("a3", A3), Map.entry("a4", A4), Map.entry("a5", A5), Map.entry("a6", A6), Map.entry("a7", A7), Map.entry("a8", A8),
+            Map.entry("b1", B1), Map.entry("b2", B2), Map.entry("b3", B3), Map.entry("b4", B4), Map.entry("b5", B5), Map.entry("b6", B6), Map.entry("b7", B7), Map.entry("b8", B8),
+            Map.entry("c1", C1), Map.entry("c2", C2), Map.entry("c3", C3), Map.entry("c4", C4), Map.entry("c5", C5), Map.entry("c6", C6), Map.entry("c7", C7), Map.entry("c8", C8),
+            Map.entry("d1", D1), Map.entry("d2", D2), Map.entry("d3", D3), Map.entry("d4", D4), Map.entry("d5", D5), Map.entry("d6", D6), Map.entry("d7", D7), Map.entry("d8", D8),
+            Map.entry("e1", E1), Map.entry("e2", E2), Map.entry("e3", E3), Map.entry("e4", E4), Map.entry("e5", E5), Map.entry("e6", E6), Map.entry("e7", E7), Map.entry("e8", E8),
+            Map.entry("f1", F1), Map.entry("f2", F2), Map.entry("f3", F3), Map.entry("f4", F4), Map.entry("f5", F5), Map.entry("f6", F6), Map.entry("f7", F7), Map.entry("f8", F8),
+            Map.entry("g1", G1), Map.entry("g2", G2), Map.entry("g3", G3), Map.entry("g4", G4), Map.entry("g5", G5), Map.entry("g6", G6), Map.entry("g7", G7), Map.entry("g8", G8),
+            Map.entry("h1", H1), Map.entry("h2", H2), Map.entry("h3", H3), Map.entry("h4", H4), Map.entry("h5", H5), Map.entry("h6", H6), Map.entry("h7", H7), Map.entry("h8", H8)
+    );
 
 }
--- src/test/java/leo/lija/model/BoardTest.java
+++ src/test/java/leo/lija/model/BoardTest.java
@@ -1,14 +1,11 @@
 package leo.lija.model;
 
-import leo.lija.exceptions.ChessRulesException;
-import leo.lija.model.Board;
-import leo.lija.model.Piece;
-import leo.lija.model.Pos;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static leo.lija.model.Color.BLACK;
 import static leo.lija.model.Color.WHITE;
+import static leo.lija.model.Pos.*;
 import static leo.lija.model.Role.BISHOP;
 import static leo.lija.model.Role.KING;
 import static leo.lija.model.Role.KNIGHT;
@@ -16,7 +13,6 @@ import static leo.lija.model.Role.PAWN;
 import static leo.lija.model.Role.QUEEN;
 import static leo.lija.model.Role.ROOK;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BoardTest {
@@ -32,80 +28,76 @@ class BoardTest {
     @Test
     @DisplayName("Should allow piece to be placed")
     void pieceCanBePlaced() {
-        Board newBoard = board.placeAt(new Piece(WHITE, ROOK), Pos.at("e3"));
-        assertThat(newBoard.at(Pos.at("e3"))).contains(new Piece(WHITE, ROOK));
+        Board newBoard = board.placeAt(new Piece(WHITE, ROOK), E3);
+        assertThat(newBoard.at(E3)).contains(new Piece(WHITE, ROOK));
     }
 
     @Test
     @DisplayName("Should allow piece to be taken")
     void pieceCanBeTaken() {
-        Board newBoard = board.take(Pos.at("a1"));
-        assertThat(newBoard.at(Pos.at("a1"))).isEmpty();
+        Board newBoard = board.take(A1);
+        assertThat(newBoard.at(A1)).isEmpty();
     }
 
     @Test
     @DisplayName("Should position white pieces correctly")
     void whitePiecesPositionedCorrectly() {
-        assertThat(board.at(Pos.at("a1"))).contains(new Piece(WHITE, ROOK));
-        assertThat(board.at(Pos.at("b1"))).contains(new Piece(WHITE, KNIGHT));
-        assertThat(board.at(Pos.at("c1"))).contains(new Piece(WHITE, BISHOP));
-        assertThat(board.at(Pos.at("d1"))).contains(new Piece(WHITE, QUEEN));
-        assertThat(board.at(Pos.at("e1"))).contains(new Piece(WHITE, KING));
-        assertThat(board.at(Pos.at("f1"))).contains(new Piece(WHITE, BISHOP));
-        assertThat(board.at(Pos.at("g1"))).contains(new Piece(WHITE, KNIGHT));
-        assertThat(board.at(Pos.at("h1"))).contains(new Piece(WHITE, ROOK));
-        assertThat(board.at(Pos.at("a2"))).contains(new Piece(WHITE, PAWN));
-        assertThat(board.at(Pos.at("b2"))).contains(new Piece(WHITE, PAWN));
-        assertThat(board.at(Pos.at("c2"))).contains(new Piece(WHITE, PAWN));
-        assertThat(board.at(Pos.at("d2"))).contains(new Piece(WHITE, PAWN));
-        assertThat(board.at(Pos.at("e2"))).contains(new Piece(WHITE, PAWN));
-        assertThat(board.at(Pos.at("f2"))).contains(new Piece(WHITE, PAWN));
-        assertThat(board.at(Pos.at("g2"))).contains(new Piece(WHITE, PAWN));
-        assertThat(board.at(Pos.at("h2"))).contains(new Piece(WHITE, PAWN));
+        assertThat(board.at(A1)).contains(new Piece(WHITE, ROOK));
+        assertThat(board.at(B1)).contains(new Piece(WHITE, KNIGHT));
+        assertThat(board.at(C1)).contains(new Piece(WHITE, BISHOP));
+        assertThat(board.at(D1)).contains(new Piece(WHITE, QUEEN));
+        assertThat(board.at(E1)).contains(new Piece(WHITE, KING));
+        assertThat(board.at(F1)).contains(new Piece(WHITE, BISHOP));
+        assertThat(board.at(G1)).contains(new Piece(WHITE, KNIGHT));
+        assertThat(board.at(H1)).contains(new Piece(WHITE, ROOK));
+        assertThat(board.at(A2)).contains(new Piece(WHITE, PAWN));
+        assertThat(board.at(B2)).contains(new Piece(WHITE, PAWN));
+        assertThat(board.at(C2)).contains(new Piece(WHITE, PAWN));
+        assertThat(board.at(D2)).contains(new Piece(WHITE, PAWN));
+        assertThat(board.at(E2)).contains(new Piece(WHITE, PAWN));
+        assertThat(board.at(F2)).contains(new Piece(WHITE, PAWN));
+        assertThat(board.at(G2)).contains(new Piece(WHITE, PAWN));
+        assertThat(board.at(H2)).contains(new Piece(WHITE, PAWN));
     }
 
     @Test
     @DisplayName("Should position black pieces correctly")
     void blackPiecesPositionedCorrectly() {
-        assertThat(board.at(Pos.at("a7"))).contains(new Piece(BLACK, PAWN));
-        assertThat(board.at(Pos.at("b7"))).contains(new Piece(BLACK, PAWN));
-        assertThat(board.at(Pos.at("c7"))).contains(new Piece(BLACK, PAWN));
-        assertThat(board.at(Pos.at("d7"))).contains(new Piece(BLACK, PAWN));
-        assertThat(board.at(Pos.at("e7"))).contains(new Piece(BLACK, PAWN));
-        assertThat(board.at(Pos.at("f7"))).contains(new Piece(BLACK, PAWN));
-        assertThat(board.at(Pos.at("g7"))).contains(new Piece(BLACK, PAWN));
-        assertThat(board.at(Pos.at("h7"))).contains(new Piece(BLACK, PAWN));
-        assertThat(board.at(Pos.at("a8"))).contains(new Piece(BLACK, ROOK));
-        assertThat(board.at(Pos.at("b8"))).contains(new Piece(BLACK, KNIGHT));
-        assertThat(board.at(Pos.at("c8"))).contains(new Piece(BLACK, BISHOP));
-        assertThat(board.at(Pos.at("d8"))).contains(new Piece(BLACK, QUEEN));
-        assertThat(board.at(Pos.at("e8"))).contains(new Piece(BLACK, KING));
-        assertThat(board.at(Pos.at("f8"))).contains(new Piece(BLACK, BISHOP));
-        assertThat(board.at(Pos.at("g8"))).contains(new Piece(BLACK, KNIGHT));
-        assertThat(board.at(Pos.at("h8"))).contains(new Piece(BLACK, ROOK));
+        assertThat(board.at(A7)).contains(new Piece(BLACK, PAWN));
+        assertThat(board.at(B7)).contains(new Piece(BLACK, PAWN));
+        assertThat(board.at(C7)).contains(new Piece(BLACK, PAWN));
+        assertThat(board.at(D7)).contains(new Piece(BLACK, PAWN));
+        assertThat(board.at(E7)).contains(new Piece(BLACK, PAWN));
+        assertThat(board.at(F7)).contains(new Piece(BLACK, PAWN));
+        assertThat(board.at(G7)).contains(new Piece(BLACK, PAWN));
+        assertThat(board.at(H7)).contains(new Piece(BLACK, PAWN));
+        assertThat(board.at(A8)).contains(new Piece(BLACK, ROOK));
+        assertThat(board.at(B8)).contains(new Piece(BLACK, KNIGHT));
+        assertThat(board.at(C8)).contains(new Piece(BLACK, BISHOP));
+        assertThat(board.at(D8)).contains(new Piece(BLACK, QUEEN));
+        assertThat(board.at(E8)).contains(new Piece(BLACK, KING));
+        assertThat(board.at(F8)).contains(new Piece(BLACK, BISHOP));
+        assertThat(board.at(G8)).contains(new Piece(BLACK, KNIGHT));
+        assertThat(board.at(H8)).contains(new Piece(BLACK, ROOK));
     }
 
     @Test
     @DisplayName("Should allow piece to be move")
     void pieceCanBeMoved() {
-        Board newBoard = board.moveTo(Pos.at("e2"), Pos.at("e4"));
-        assertThat(newBoard.at(Pos.at("e2"))).isEmpty();
-        assertThat(newBoard.at(Pos.at("e4"))).contains(new Piece(WHITE, PAWN));
+        Board newBoard = board.moveTo(E2, E4);
+        assertThat(newBoard.at(E2)).isEmpty();
+        assertThat(newBoard.at(E4)).contains(new Piece(WHITE, PAWN));
     }
 
     @Test
     @DisplayName("Should not allow empty place to move")
     void emptyPlaceCannotBeMoved() {
-        Pos from = Pos.at("e5");
-        Pos to = Pos.at("e6");
-        assertThrows(Exception.class, () -> board.moveTo(from, to));
+        assertThrows(Exception.class, () -> board.moveTo(E5, E6));
     }
 
     @Test
     @DisplayName("Should not allow to move to occupied position")
     void canNotBeMoveToOccupiedPosition() {
-        Pos from = Pos.at("a1");
-        Pos to = Pos.at("a2");
-        assertThrows(Exception.class, () -> board.moveTo(from, to));
+        assertThrows(Exception.class, () -> board.moveTo(A1, A2));
     }
 }
