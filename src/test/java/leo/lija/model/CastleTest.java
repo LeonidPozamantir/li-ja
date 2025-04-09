8054c28d3cf6e53e795c53aa4219d2e702198ed8
Pozamantir <lp029g@intl.att.com>
2025-04-09 12:58:45 +0300

    more castle tests

--- src/test/java/leo/lija/model/CastleTest.java
+++ src/test/java/leo/lija/model/CastleTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static leo.lija.model.Color.WHITE;
+import static leo.lija.model.Pos.B1;
 import static leo.lija.model.Pos.C1;
 import static leo.lija.model.Pos.D1;
 import static leo.lija.model.Pos.E1;
@@ -15,33 +17,57 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("a king should castle")
 public class CastleTest {
 
+	private void beSituation(Situation situation, String visualString) {
+		assertThat(visual.obj2Str(situation.board))
+			.isEqualTo(visual.obj2Str(visual.str2Obj(visualString)));
+	}
+
 	VisualFormat visual = new VisualFormat();
 	@Nested
 	@DisplayName("king side")
 	class Kingside {
+
+		Board badHist = visual.str2Obj("""
+PPPPPPPP
+R  QK  R""").withHistory(History.castle(WHITE, false, false));
+		Board goodHist = badHist.withHistory(History.castle(WHITE, true, true));
 
 		@Nested
 		@DisplayName("impossible")
 		class Impossible {
 
 			@Test
-			@DisplayName("pieces in the way")
-			void piecesInTheWay() {
-				assertThat(new Board().movesFrom(E1)).isEmpty();
+			@DisplayName("bishop in the way")
+			void bishopInTheWay() {
+				assertThat(goodHist.placeAt(WHITE.bishop(), F1).movesFrom(E1)).isEmpty();
+			}
+			@Test
+			@DisplayName("knight in the way")
+			void knightInTheWay() {
+				assertThat(goodHist.placeAt(WHITE.knight(), G1).movesFrom(E1)).containsExactly(F1);
+			}
+			@Test
+			@DisplayName("not allowed by history")
+			void badHistory() {
+				assertThat(badHist.movesFrom(E1)).containsExactly(F1);
 			}
 		}
 
 		@Nested
 		@DisplayName("possible")
 		class Possible {
-
-			Board board = visual.str2Obj("""
-PPPPPPPP
-R  QK  R""");
 			@Test
 			@DisplayName("viable moves")
 			void viableMoves() {
-				assertThat(board.movesFrom(E1)).containsExactlyInAnyOrder(F1, G1);
+				assertThat(goodHist.movesFrom(E1)).containsExactlyInAnyOrder(F1, G1);
+			}
+
+			@Test
+			@DisplayName("correct new board")
+			void correctNewBoard() {
+				beSituation(goodHist.as(WHITE).playMove(E1, G1), """
+PPPPPPPP
+R  Q RK """);
 			}
 		}
 	}
@@ -51,28 +77,52 @@ R  QK  R""");
 	@DisplayName("queen side")
 	class Queenside {
 
+		Board badHist = visual.str2Obj("""
+PPPPPPPP
+R   KB R""").withHistory(History.castle(WHITE, false, false));
+		Board goodHist = badHist.withHistory(History.castle(WHITE, true, true));
+
 		@Nested
 		@DisplayName("impossible")
 		class Impossible {
 
 			@Test
-			@DisplayName("pieces in the way")
-			void piecesInTheWay() {
-				assertThat(new Board().movesFrom(E1)).isEmpty();
+			@DisplayName("queen in the way")
+			void queenInTheWay() {
+				assertThat(goodHist.placeAt(WHITE.queen(), D1).movesFrom(E1)).isEmpty();
+			}
+			@Test
+			@DisplayName("bishop in the way")
+			void bishopInTheWay() {
+				assertThat(goodHist.placeAt(WHITE.bishop(), C1).movesFrom(E1)).containsExactly(D1);
+			}
+			@Test
+			@DisplayName("knight in the way")
+			void knightInTheWay() {
+				assertThat(goodHist.placeAt(WHITE.knight(), B1).movesFrom(E1)).containsExactly(D1);
+			}
+			@Test
+			@DisplayName("not allowed by history")
+			void badHistory() {
+				assertThat(badHist.movesFrom(E1)).containsExactly(D1);
 			}
 		}
 
 		@Nested
 		@DisplayName("possible")
 		class Possible {
-
-			Board board = visual.str2Obj("""
-PPPPPP
-R   KB""");
 			@Test
 			@DisplayName("viable moves")
 			void viableMoves() {
-				assertThat(board.movesFrom(E1)).containsExactlyInAnyOrder(D1, C1);
+				assertThat(goodHist.movesFrom(E1)).containsExactlyInAnyOrder(D1, B1);
+			}
+
+			@Test
+			@DisplayName("correct new board")
+			void correctNewBoard() {
+				beSituation(goodHist.as(WHITE).playMove(E1, B1), """
+PPPPPPPP
+  KR B R""");
 			}
 		}
 	}
