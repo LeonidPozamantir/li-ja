ce1e817de6a26e0a2f4334a806d52ad693e1cfde
LeonidPozamantir <leonid.pozamantir@gmail.com>
2025-06-13 18:44:47 +0300

    removed src from the root directory

--- src/main/java/leo/lija/exceptions/ChessRulesException.java
+++
@@ -1,8 +0,0 @@
-package leo.lija.exceptions;
-
-public class ChessRulesException extends RuntimeException {
-
-    public ChessRulesException(String message) {
-        super(message);
-    }
-}
--- src/main/java/leo/lija/repo/GameRepo.java
+++
@@ -1,7 +0,0 @@
-package leo.lija.repo;
-
-import leo.lija.entities.Game;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface GameRepo extends MongoRepository<Game, String> {
-}
