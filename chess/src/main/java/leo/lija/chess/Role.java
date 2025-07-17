b6a96b4a6ebdb68de4f4ea627c9306d7ebb125de
LeonidPozamantir <leonid.pozamantir@gmail.com>
2025-07-17 16:49:19 +0300

    add convenience methods in Role object

--- chess/src/main/java/leo/lija/chess/Role.java
+++ chess/src/main/java/leo/lija/chess/Role.java
@@ -2,9 +2,12 @@ package leo.lija.chess;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public enum Role {
@@ -25,6 +28,16 @@ public enum Role {
     PAWN('p', false,null);
 
     public static final List<Role> all = List.of(KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN);
+    public static final List<Role> allPromotable = List.of(QUEEN, ROOK, BISHOP, KNIGHT);
+    public static final Map<String, Role> allPromotableByName = Collections.unmodifiableMap(allPromotable.stream().collect(Collectors.toMap(Role::toString, Function.identity())));
+
+    public Optional<Role> promotable(String name) {
+        return Optional.ofNullable(allPromotableByName.get(name));
+    }
+
+    public Optional<Role> promotable(Optional<String> name) {
+        return name.flatMap(n -> promotable(n));//.orElse(Optional.of(QUEEN)); - incorrect, fill be fixed 11/05
+    }
 
     public final char fen;
     public final boolean promotable;
