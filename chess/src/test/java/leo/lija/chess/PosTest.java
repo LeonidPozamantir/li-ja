4903ba6c1a5682baa54636813f60534d701ef5d1
LeonidPozamantir <leonid.pozamantir@gmail.com>
2025-07-30 23:06:57 +0300

    remove deprecated tests

--- chess/src/test/java/leo/lija/chess/PosTest.java
+++ chess/src/test/java/leo/lija/chess/PosTest.java
@@ -27,42 +27,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class PosTest {
 
     @Test
-    @DisplayName("be used to derive a relative position on the board")
-    void relativePosition() {
-        assertEquals(Optional.of(D6), D5.shiftUp(1));
-        assertEquals(Optional.of(D4), D5.shiftDown(1));
-        assertEquals(Optional.of(C5), D5.shiftLeft(1));
-        assertEquals(Optional.of(E5), D5.shiftRight(1));
-    }
-
-    @Test
-    @DisplayName("be used to calculate a non-position off the edge of the board")
-    void offTheBoard() {
-        assertEquals(Optional.empty(), D5.shiftUp(4));
-        assertEquals(Optional.empty(), D5.shiftDown(5));
-        assertEquals(Optional.empty(), D5.shiftLeft(5));
-        assertEquals(Optional.empty(), D5.shiftRight(5));
-    }
-
-    @Test
-    @DisplayName("be able to calculate a relative position with negative numbers")
-    void relativePositionWithNegative() {
-        assertEquals(Optional.of(D3), D5.shiftUp(-2));
-        assertEquals(Optional.of(D8), D5.shiftDown(-3));
-        assertEquals(Optional.of(G5), D5.shiftLeft(-3));
-        assertEquals(Optional.of(C5), D5.shiftRight(-1));
-    }
-
-    @Test
-    @DisplayName("be used to calculate a non-position off the edge of the board using negative numbers")
-    void offTheBoardWithNegative() {
-        assertEquals(Optional.empty(), D5.shiftUp(-6));
-        assertEquals(Optional.empty(), D5.shiftDown(-6));
-        assertEquals(Optional.empty(), D5.shiftLeft(-6));
-        assertEquals(Optional.empty(), D5.shiftRight(-6));
-    }
-
-    @Test
     @DisplayName("be used to derive a relative list of positions")
     void relativeList() {
         assertThat(D4.multShiftLeft((p) -> false)).containsExactlyInAnyOrder(C4, B4, A4);
