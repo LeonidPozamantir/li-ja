a95277916c47df1bf52d434ab8ad150f9faa618b
LeonidPozamantir <leonid.pozamantir@gmail.com>
2025-11-30 21:20:16 +0200

    user java api to get the system load average

--- app/src/main/java/leo/lija/app/reporting/Reporting.java
+++ app/src/main/java/leo/lija/app/reporting/Reporting.java
@@ -8,6 +8,10 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class Reporting {
@@ -24,14 +28,13 @@ public class Reporting {
     @Getter
     private long nbPlaying = 0;
     private long nbGameSockets = 0;
+    private Optional<Float> loadAvg = Optional.empty();
     private boolean remoteAi = false;
+
+    private OperatingSystemMXBean osStats = ManagementFactory.getOperatingSystemMXBean();
 
     public String getStatus() {
         return status();
-    }
-
-    private String status() {
-        return String.join(" ", String.valueOf(nbMembers), String.valueOf(nbGames), String.valueOf(nbPlaying), String.valueOf(nbGameSockets), remoteAi ? "1" : "0");
     }
 
     public void update() {
@@ -39,6 +42,24 @@ public class Reporting {
         nbGames = gameRepo.countAll();
         nbPlaying = gameRepo.countPlaying();
         nbGameSockets = gameHubMemo.count();
+        loadAvg = getLoadAvg();
         remoteAi = remoteAiService.currentHealth();
     }
+
+    private Optional<Float> getLoadAvg() {
+        return Optional.of((float) osStats.getSystemLoadAverage());
+    }
+
+    private String status() {
+        return String.join(" ",
+            String.valueOf(nbMembers),
+            String.valueOf(nbGames),
+            String.valueOf(nbPlaying),
+            String.valueOf(nbGameSockets),
+            loadAvg.map(String::valueOf).orElse("?"),
+            remoteAi ? "1" : "0"
+        );
+    }
+
+
 }
