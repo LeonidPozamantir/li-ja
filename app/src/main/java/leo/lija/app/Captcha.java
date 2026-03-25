cf19a41451f0cddf1577647b5ecde81a952164fe
Leonid Pozamantir <leonid.pozamantir@gmail.com>
2026-03-25 20:23:11 +0200

    better captcha rewind error messages

--- app/src/main/java/leo/lija/app/Captcha.java
+++ app/src/main/java/leo/lija/app/Captcha.java
@@ -2,6 +2,7 @@ package leo.lija.app;
 
 import io.vavr.Tuple3;
 import leo.lija.app.db.GameRepo;
+import leo.lija.app.entities.DbGame;
 import leo.lija.app.exceptions.AppException;
 import leo.lija.chess.Color;
 import leo.lija.chess.Game;
@@ -23,14 +24,14 @@ public class Captcha {
     public Tuple3<String, String, Color> create() {
         return gameRepo.findOneStandardCheckmate()
             .map(game -> {
-                Game rewinded = rewind(game.getPgn());
+                Game rewinded = rewind(game);
                 return new Tuple3<>(game.getId(), fen(rewinded), rewinded.getPlayer());
             }).orElseThrow(() -> new AppException("No checkmate available"));
     }
 
     public List<String> solve(String id) {
         return gameRepo.game(id).map(game -> {
-                Game rewinded = rewind(game.getPgn());
+                Game rewinded = rewind(game);
                 List<String> moves = mateMoves(rewinded);
                 if (!moves.isEmpty()) return moves;
                 throw new AppException("No solution found");
@@ -47,9 +48,13 @@ public class Captcha {
             .toList();
     }
 
-    private Game rewind(String pgn) {
-        return PgnReader.withSans(pgn, l -> l.subList(0, l.size() - 1))
-            .game();
+    private Game rewind(DbGame game) {
+        try {
+            return PgnReader.withSans(game.getPgn(), l -> l.subList(0, l.size() - 1))
+                .game();
+        } catch (AppException e) {
+            throw failInfo(game, e);
+        }
     }
 
     private String fen(Game game) {
@@ -58,4 +63,7 @@ public class Captcha {
         return idx == -1 ? f : f.substring(0, idx);
     }
 
+    private AppException failInfo(DbGame game, Exception e) {
+        return new AppException("Rewind %s".formatted(game.getId()), e);
+    }
 }
